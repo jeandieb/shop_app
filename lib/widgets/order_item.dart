f772ef0fe4bab6c24702f8f91abca12d9fa86f3e
jeandieb <johnnydib15@gmail.com>
2021-07-29 20:14:39 -0700

    finished orders and orders screen

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -4,39 +4,82 @@ import 'package:intl/intl.dart';
 
 import '../providers/orders.dart' as OP; //Orders Provider
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   @required
   final orderIndex;
 
   OrderItem(this.orderIndex);
 
   @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  bool _isExpanded = false;
+  @override
   Widget build(BuildContext context) {
     List<OP.OrderItem> orders = Provider.of<OP.Orders>(context).orders;
-    return orders[orderIndex].isOrderEmpty()
+    return orders[widget.orderIndex].isOrderEmpty()
         ? Container(
             width: 0.0,
             height:
-                0.0) //empty invisible container that takes as less space as poss.
+                0.0) //empty invisible container that takes as less space as possible
+
         : Card(
             margin: EdgeInsets.all(10),
             child: Column(
-              children: [
+              children: <Widget>[
                 ListTile(
-                  title: Text('\$${orders[orderIndex].total}'),
+                  title: Text('\$${orders[widget.orderIndex].total}'),
                   subtitle: Text(
-                    DateFormat('MM/dd/yyyy hh:mm')
-                        .format(orders[orderIndex].orderDate),
+                    DateFormat('dd/MM/yyyy hh:mm')
+                        .format(orders[widget.orderIndex].orderDate),
                   ),
                   trailing: IconButton(
-                    icon: Icon(Icons.expand_more),
-                    onPressed: (){
-                      
+                    icon: Icon(
+                        _isExpanded ? Icons.expand_less : Icons.expand_more),
+                    onPressed: () {
+                      setState(() {
+                        _isExpanded = !_isExpanded;
+                      });
                     },
                   ),
                 ),
+                if (_isExpanded)
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+                    height: (100),
+                    child: ListView(
+                      children: orders[widget.orderIndex]
+                          .items
+                          .map(
+                            (prod) => Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text(
+                                  prod.title,
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  '${prod.quantity}x \$${prod.price}',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.grey,
+                                  ),
+                                )
+                              ],
+                            ),
+                          )
+                          .toList(),
+                    ),
+                  )
               ],
             ),
           );
+
+    
   }
 }
