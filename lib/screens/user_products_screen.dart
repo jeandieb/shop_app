c8a685d313a4e8887f98b115a5961a9a68674fff
jeandieb <johnnydib15@gmail.com>
2021-08-04 20:25:30 -0700

    added pull down refresh indicator

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -7,9 +7,15 @@ import '../screens/edit_product_screen.dart';
 
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user_products';
+
+  Future<void> _refreshProducts(BuildContext context)  async
+  {
+    await Provider.of<Products>(context).fetchAndSetProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final productsData = Provider.of<Products>(context);
+    final productsData = Provider.of<Products>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text('My Products'),
@@ -19,19 +25,22 @@ class UserProductsScreen extends StatelessWidget {
           }, icon: Icon(Icons.add)),
         ],
       ),
-      body: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: ListView.builder(
-            itemCount: productsData.items.length,
-            itemBuilder: (_, index) {
-              return Column(
-                children: [
-                  UserProduct(productsData.items[index].title,
-                      productsData.items[index].imageUrl, productsData.items[index].id),
-                  Divider(color: Theme.of(context).primaryColor, thickness: 1,),
-                ],
-              );
-            }),
+      body: RefreshIndicator(
+          onRefresh: () => _refreshProducts(context),
+              child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: ListView.builder(
+              itemCount: productsData.items.length,
+              itemBuilder: (_, index) {
+                return Column(
+                  children: [
+                    UserProduct(productsData.items[index].title,
+                        productsData.items[index].imageUrl, productsData.items[index].id),
+                    Divider(color: Theme.of(context).primaryColor, thickness: 1,),
+                  ],
+                );
+              }),
+        ),
       ),
     );
   }
